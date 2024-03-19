4834e4fde3907d9cbffdf2b6df0e028c0fabd5aa
Max <maxcatalina@iMac-Max.local>
2024-03-19 10:22:13 +0200

    w

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-const array = [1, 2, 3, 4, 7, 5, 6, 5, 8, 5, 7, 6];
-
-function deleteFromАrray(arr, ...items) {
-    console.log(`Числа які будуть видалені з наступного масиву ${items}`);
-    return arr.filter(el => !items.includes(el));
+function getPow(num, degree) {
+    if (degree > 1) {
+        return num * getPow(num, degree - 1);
+    }
+    return num;
 }
 
-const newArray = deleteFromАrray(array, 5, 4);
-
-console.log(newArray);
-console.log(deleteFromАrray(array, 7, 6));
-console.log(deleteFromАrray(array, 1, 2, 3, 4))
+console.log(getPow(3,9));
